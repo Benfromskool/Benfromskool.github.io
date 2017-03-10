92730a38a7944c784b87092703a59d79ea40e43c
bnadrich <bnadrich@newroads.org>
2017-03-10 12:17:58 -0500

    clean

--- calc.js
+++ calc.js
@@ -44,4 +44,41 @@ function test(){
   
   }
   
+  if(type == "takeout" && quality == "great"){
+   alert("Recommended Tip: 15%");
+  
+  }
+  
+ else if(type == "takeout" && quality == "good"){
+   alert("Recommended Tip: 10%");
+  
+  }
+  
+  else if(type == "takeout" && quality == "mediocre"){
+   alert("Recommended Tip: 5%");
+  
+  }
+  
+  else if(type == "takeout" && quality == "bad"){
+   alert("Recommended Tip: 0%");
+  }
+  
+  if(type == "Family" && quality == "great"){
+   alert("Recommended Tip: 25%");
+  
+  }
+  
+ else if(type == "Family" && quality == "good"){
+   alert("Recommended Tip: 15%");
+  
+  }
+  
+  else if(type == "Family" && quality == "mediocre"){
+   alert("Recommended Tip: 10%");
+  
+  }
+  
+  else if(type == "Family" && quality == "bad"){
+   alert("Recommended Tip: 0%");
+  }
 }
